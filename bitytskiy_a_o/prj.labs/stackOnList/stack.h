c93797a3a861b581813eb5547474b5c54a5b3c65
Andrew <applofilll@gmail.com>
2018-11-13 13:43:42 +0300

    fix stack

--- bitytskiy_a_o/prj.labs/stackOnList/stack.h
+++ bitytskiy_a_o/prj.labs/stackOnList/stack.h
@@ -12,78 +12,21 @@ public:
 		clear();
 	}
 
-	StackOnList(const StackOnList& obj) {
-		Node<T>* newNode = obj.head_;
-		StackOnList<T> otherStack;
-		while (newNode != nullptr) {
-			otherStack.push(newNode->data_);
-			newNode = newNdoe->next_;
-		}
-		while (!otherStack.isEmpty()) {
-			push(otherStack.head_->data_);
-			otherStack.pop();
-		}
-	}
+	StackOnList(const StackOnList& obj);
 
-	StackOnList& operator=(const StackOnList& rhs) {
-		if (this != &rhs) {
-			clear();
-			Node<T>* newNode = rhs.head_;
-			StackOnList<T> otherStack;
-			while (newNode != nullptr) {
-				otherStack.push(newNode->data_);
-				newNode = newNode->next_;
-			}
-			while (!otherStack.isEmpty()) {
-				push(otherStack.head_->data_);
-				otherStack.pop();
-			}
-		}
-		return *this;
-	}
+	StackOnList<T>& operator=(const StackOnList<T>& rhs);
 
-	void push(const T& value) {
-		head_ = new Node<T>(head_, value);
-	}
+	void push(const T& value);
 
-	void pop(){
-		if (nullptr != head_) {
-			Node<T>* deletedNode(head_);
-			head_ = head_->next_;
-			delete deletedNode;
-		}
-	}
+	void pop();
 
-	bool isEmpty() {
-		return head_ == nullptr;
-	}
+	bool isEmpty();
 
-	void clear() {
-		while (!isEmpty()) {
-			pop();
-		}
-	}
+	void clear();
 
-	T& top() {
-		if (head_ == nullptr) {
-			throw std::exception("Stack is empty");
-		}
-		return head_->data_;
-	}
+	T& top();
 
-	std::ostream& WriteTo(std::ostream& ostrm) const {
-		Node<T>* printed_node(head_);
-		ostrm << '{';
-		while (printed_node) {
-			ostrm << printed_node->data_;
-			printed_node = printed_node->next_;
-			if (printed_node) {
-				ostrm << ", ";
-			}
-		}
-		ostrm << '}';
-		return ostrm;
-	}
+	std::ostream& WriteTo(std::ostream& ostrm) const;
 
 private:
 	template<class A>
@@ -100,6 +43,86 @@ private:
 	Node<T>* head_{ nullptr };
 };
 
+template<class T>
+StackOnList<T>::StackOnList(const StackOnList& obj) {
+	Node<T>* newNode = obj.head_;
+	StackOnList<T> otherStack;
+	while (newNode != nullptr) {
+		otherStack.push(newNode->data_);
+		newNode = newNdoe->next_;
+	}
+	while (!otherStack.isEmpty()) {
+		push(otherStack.head_->data_);
+		otherStack.pop();
+	}
+}
+
+template<class T>
+StackOnList<T>& StackOnList<T>::operator=(const StackOnList<T>& rhs) {
+	if (this != &rhs) {
+		clear();
+		Node<T>* newNode = rhs.head_;
+		StackOnList<T> otherStack;
+		while (newNode != nullptr) {
+			otherStack.push(newNode->data_);
+			newNode = newNode->next_;
+		}
+		while (!otherStack.isEmpty()) {
+			push(otherStack.head_->data_);
+			otherStack.pop();
+		}
+	}
+	return *this;
+}
+
+template<class T>
+void StackOnList<T>::push(const T& value) {
+	head_ = new Node<T>(head_, value);
+}
+
+template<class T>
+void StackOnList<T>::pop() {
+	if (nullptr != head_) {
+		Node<T>* deletedNode(head_);
+		head_ = head_->next_;
+		delete deletedNode;
+	}
+}
+
+template<class T>
+bool StackOnList<T>::isEmpty() {
+	return head_ == nullptr;
+}
+
+template<class T>
+void StackOnList<T>::clear() {
+	while (!isEmpty()) {
+		pop();
+	}
+}
+
+template<class T>
+T& StackOnList<T>::top() {
+	if (head_ == nullptr) {
+		throw std::exception("Stack is empty");
+	}
+	return head_->data_;
+}
+
+template<class T>
+std::ostream& StackOnList<T>::WriteTo(std::ostream& ostrm) const{
+	Node<T>* printed_node(head_);
+	ostrm << '{';
+	while (printed_node) {
+		ostrm << printed_node->data_;
+		printed_node = printed_node->next_;
+		if (printed_node) {
+			ostrm << ", ";
+		}
+	}
+	ostrm << '}';
+	return ostrm;
+}
 std::ostream& operator<<(std::ostream& ostrm, const StackOnList<int>& stack) {
 	return stack.WriteTo(ostrm);
 }
